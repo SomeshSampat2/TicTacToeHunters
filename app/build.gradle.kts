7ce9c66477858f3dddf845f459f4bfbced7bda75
SomeshSampat2 <someshsampat1234@gmail.com>
2024-11-18 09:33:33 +0530

    Release 1.2 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.innovatelabz.tictactoehunters"
         minSdk = 24
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.1"
+        versionCode = 12
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
